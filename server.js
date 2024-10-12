1e3e9a6d8cdbf6968cb1b950455fc2345ff78603
sudarshan233 <nsridharsudarshan@gmail.com>
2024-10-12 06:12:54 +0530

    Output Video uploaded

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.urlencoded({extended:true}))
 
 mongoose.connect('mongodb://127.0.0.1:27017/basic')
 const db=mongoose.connection
+
 db.once('open',()=>{
     console.log("MongoDB connection successful")
 })
